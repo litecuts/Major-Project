4c79ba01474335af03baf3f3f2fde8e95f181ce0
litecuts <71206543+litecuts@users.noreply.github.com>
2021-02-11 12:33:50 -0600

    Made the starting page

--- sketch.js
+++ sketch.js
@@ -6,11 +6,24 @@
 // - describe what you did to take this project "above and beyond"
 
 
+let playerX = 0;
+let playerY = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noStroke();
+  rectMode(CENTER);
 }
 
 function draw() {
-  background(220);
-}
+  background("black");
+
+  let r1 = map(mouseX, 0, width, 0, height);
+  let r2 = height - r1;
+
+  fill(255, 0, 100, r1);
+  rect(width / 2 + r1 / 2, height / 2, r1, r1);
+
+  fill(255, 0, 100, r2);
+  rect(width / 2 - r2 / 2, height / 2, r2, r2);
+}   
